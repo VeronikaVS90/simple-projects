b0d228ceb3ef605fbc57607fc6c2c0e43b734644
VeronikaVS90 <vsokolovanika@gmail.com>
2025-09-25 20:35:15 +0300

    add game Guess the number

---
+++ calculator/script.js
@@ -0,0 +1,43 @@
+function append(char) {
+    document.getElementById('display').value += char;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+    
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        alert("Invalid expression");
+        display.value = '';
+    }
+}
+
+// keyboard improvement
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    const validKeys = "0123456789+-*/.";
+
+    if (validKeys.includes(key)) {
+        append(key);
+    } else if (key === "Enter") {
+        calculate();
+    } else if (key === "Backspace") {
+        deleteLast();
+    } else if (key === "Escape") {
+        clearDisplay();
+    }
+});
+
+window.onload = function () {
+    document.getElementById("display").focus();
+}
